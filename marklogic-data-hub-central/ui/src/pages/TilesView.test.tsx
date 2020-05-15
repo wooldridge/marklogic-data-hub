41622c3f6a54ef5290da99db2e2dfc9de58a7bf2
wooldridge <mike@hellion.org>
2020-05-15 16:20:11 -0700

    DHFPROD-4772: Fix Run tile test
    Pass in authorities context to Run tile test.

--- marklogic-data-hub-central/ui/src/pages/TilesView.test.tsx
+++ marklogic-data-hub-central/ui/src/pages/TilesView.test.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import { render, fireEvent, waitForElement, cleanup, getAllByRole } from '@testing-library/react'
+import { render, fireEvent, waitForElement, cleanup } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import TilesView from './TilesView';
+import {AuthoritiesContext} from '../util/authorities';
 import axiosMock from 'axios';
 import curateData from '../config/bench.config';
+import authorities from '../config/authorities.config';
 
 jest.mock('axios');
+
+const mockDevRolesService = authorities.DeveloperRolesService;
 
 describe('TilesView component', () => {
 
@@ -15,7 +19,7 @@ describe('TilesView component', () => {
     })
     
     test('Verify TilesView renders with the toolbar', async () => {
-        const {getByLabelText, getAllByRole, debug} = render(<TilesView/>);
+        const {getByLabelText} = render(<TilesView/>);
 
         expect(getByLabelText("toolbar")).toBeInTheDocument();
 
@@ -59,7 +63,7 @@ describe('TilesView component', () => {
     });
 
     test('Verify Run tile displays from toolbar', async () => {
-        const {getByLabelText, getByText, queryByText} = render(<TilesView/>);
+        const {getByLabelText, getByText, queryByText} = await render(<AuthoritiesContext.Provider value={ mockDevRolesService}><TilesView/></AuthoritiesContext.Provider>);
 
         // Run tile not shown initially
         expect(queryByText("icon-run")).not.toBeInTheDocument();
@@ -84,7 +88,7 @@ describe('TilesView component', () => {
         expect(getByLabelText("title-run")).toBeInTheDocument();
         expect(document.querySelector('#flows-container')).toBeInTheDocument();
         expect(getByText('Create Flow')).toBeInTheDocument();
-        expect(getByText('test-flow')).toBeInTheDocument();
+        expect(getByText('testFlow')).toBeInTheDocument();
     });
 
 });
