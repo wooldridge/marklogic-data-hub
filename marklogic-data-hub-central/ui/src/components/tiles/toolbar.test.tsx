f4da42b1bcdaaa88dd9e3dd174eb35a5b2ecbc5f
Jaker Yang <jyang@hpzbookg5-6095.marklogic.com>
2020-11-19 12:43:12 -0600

    DHFPROD-5650: added keyboard controls to tiles toolbar

--- marklogic-data-hub-central/ui/src/components/tiles/toolbar.test.tsx
+++ marklogic-data-hub-central/ui/src/components/tiles/toolbar.test.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Router } from 'react-router';
 import { createMemoryHistory } from 'history';
 const history = createMemoryHistory();
+import userEvent from '@testing-library/user-event'
 import { render, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Toolbar from './toolbar';
@@ -44,4 +45,58 @@ describe('Toolbar component', () => {
         });
     });
 
+    it('verify tabbing of tile icons', () => {
+        var i: int;
+        let enabledTiles = ['load', 'model', 'curate', 'run', 'explore'];
+        const {getByLabelText} = render(<Router history={history}><Toolbar tiles={tiles} enabled={enabledTiles}/></Router>);
+        expect(getByLabelText("toolbar")).toBeInTheDocument();
+
+        // focus on load tool
+        getByLabelText("tool-load").focus();
+        expect(getByLabelText("tool-load")).toHaveFocus();
+
+        // press tab and verify each tool is selected in turn
+        for(i = 1; i < 5; ++i) {
+            userEvent.tab();
+            expect(getByLabelText("tool-" + enabledTiles[i])).toHaveFocus();
+        }
+
+        // press shift+tab and verify focus reverses direction
+        for(i = 3; i >= 0; --i) {
+            userEvent.tab({shift: true});
+            expect(getByLabelText("tool-" + enabledTiles[i])).toHaveFocus();
+        }
+    });
+
+
+    it('verify tabbing of tile icons', () => {
+        var i: int;
+        let enabledTiles = ['load', 'model', 'curate', 'run', 'explore'];
+        const {getByLabelText} = render(<Router history={history}><Toolbar tiles={tiles} enabled={enabledTiles}/></Router>);
+        expect(getByLabelText("toolbar")).toBeInTheDocument();
+
+        // focus on load tool
+        getByLabelText("tool-load").focus();
+        expect(getByLabelText("tool-load")).toHaveFocus();
+
+        // pressing up arrow while on load does nothing (cannot go further up)
+        fireEvent.keyDown(getByLabelText("tool-load"), { key: 'ArrowUp', code: 'ArrowUp' })
+        expect(getByLabelText("tool-load")).toHaveFocus();
+
+        // pressing down arrow sequentially moves focus down
+        for(i = 1; i < 5; ++i) {
+            fireEvent.keyDown(getByLabelText("tool-" + enabledTiles[i-1]), { key: 'ArrowDown', code: 'ArrowDown' })
+            expect(getByLabelText("tool-" + enabledTiles[i])).toHaveFocus();    
+        }
+
+        // pressing down arrow while on explore does nothing (cannot go further down)
+        fireEvent.keyDown(getByLabelText("tool-explore"), { key: 'ArrowDown', code: 'ArrowDown' })
+        expect(getByLabelText("tool-explore")).toHaveFocus();
+
+        // pressing up arrow sequentially moves focus up
+        for(i = 3; i >= 0; --i) {
+            fireEvent.keyDown(getByLabelText("tool-" + enabledTiles[i+1]), { key: 'ArrowUp', code: 'ArrowUp' })
+            expect(getByLabelText("tool-" + enabledTiles[i])).toHaveFocus();    
+        }
+    });
 });
--- marklogic-data-hub-central/ui/src/components/tiles/toolbar.tsx
+++ marklogic-data-hub-central/ui/src/components/tiles/toolbar.tsx
@@ -1,5 +1,5 @@
 import React, { CSSProperties } from 'react';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import styles from './toolbar.module.scss';
@@ -14,7 +14,13 @@ interface Props {
 
 const Toolbar: React.FC<Props> = (props) => {
 
+    const history = useHistory();
+
     const tiles = props.tiles; // config/tiles.config.ts
+
+    // array of references used to set focus
+    let tileRefs = new Array();
+    for (var i = 0; i < Object.keys(tiles).length; ++i ) tileRefs.push(React.createRef<HTMLDivElement>());    
 
     const getTooltip = (id) => {
         if (props.enabled && props.enabled.includes(id)) {
@@ -36,53 +42,63 @@ const Toolbar: React.FC<Props> = (props) => {
         return (props.enabled && props.enabled.includes(id)) ? enabled : disabled;
     };
 
+    const tileKeyDownHandler = (event, id, index) => {
+        if(event.key == 'Enter' && props.enabled && props.enabled.includes(id)) history.push(`/tiles/${id}`)
+        if(event.key == 'ArrowUp' && index > 0) tileRefs[index-1].current.focus()
+        if(event.key == 'ArrowDown' && index < (Object.keys(tiles).length - 1)) tileRefs[index+1].current.focus()
+    };
+
+    const tileOnClickHandler = (id) => {
+        if (props.enabled && props.enabled.includes(id)) history.push(`/tiles/${id}`)
+    };
+
+    const tileOnFocusHandler = (id, index) => {
+        //tileRefs[index].current.style.color = 'rgb(255, 0, 0)';
+        //tileRefs[index].current.style.border = "1px solid red";
+    };
+
+    const tileOnBlurHandler = (id, index) => {
+        //tileRefs[index].current.style.color = tiles[id]['color'];
+        //tileRefs[index].current.style.border = null;
+    }
+
     return (
         <div id={styles.toolbar} aria-label={'toolbar'}>
             {Object.keys(tiles).map((id, i) => {
                 if (tiles[id]['iconType'] === 'custom') {
                     return (
-                        props.enabled && props.enabled.includes(id) ?
-                            <Link to={
-                                {pathname: `/tiles/${id}`,
-                                state: {
-                                    tileIconClicked : true
-                                }}} aria-label={'tool-' + id + '-link'} key={i}>
-                                <MLTooltip title={getTooltip(id)} placement="leftTop" key={i}>
-                                    <div
-                                        className={tiles[id]['icon']}
-                                        aria-label={'tool-' + id}
-                                        style={getIconStyle(id)}
-                                    />
-                                </MLTooltip>
-                            </Link>
-                            :
+                        <div aria-label={'tool-' + id + '-link'} key={i} tabIndex={-1}>
                             <MLTooltip title={getTooltip(id)} placement="leftTop" key={i}>
                                 <div
                                     className={tiles[id]['icon']}
                                     aria-label={'tool-' + id}
                                     style={getIconStyle(id)}
+                                    onFocus={(e) => tileOnFocusHandler(id, i)}
+                                    onBlur={(e) => tileOnBlurHandler(id, i)}
+                                    tabIndex={1}
+                                    ref={tileRefs[i]}
+                                    onClick={(e) => tileOnClickHandler(id)}
+                                    onKeyDown={(e) => tileKeyDownHandler(e, id, i)}
                                 />
                             </MLTooltip>
+                        </div>
                     );
                 } else {
                     return (
                         <MLTooltip title={getTooltip(id)} placement="leftTop" key={i}>
-                            {props.enabled && props.enabled.includes(id) ?
-                                <Link to={'/tiles/' + id} aria-label={'tool-' + id + '-link'} >
-                                    <i
-                                        className={styles.tool}
-                                        aria-label={'tool-' + id}
-                                        style={getIconStyle(id)}
-                                    ><FontAwesomeIcon icon={tiles[id]['icon']} size="lg" />
-                                    </i>
-                                </Link>
-                                :
+                            <div aria-label={'tool-' + id + '-link'} tabIndex={-1}>
                                 <i
                                     className={styles.tool}
                                     aria-label={'tool-' + id}
                                     style={getIconStyle(id)}
-                                ><FontAwesomeIcon icon={tiles[id]['icon']} size="lg" />
-                                </i>}
+                                    tabIndex={1}
+                                    ref={tileRefs[i]}
+                                    onClick={(e) => tileOnClickHandler(id)}
+                                    onKeyDown={(e) => tileKeyDownHandler(e, id, i)}
+                                >
+                                    <FontAwesomeIcon icon={tiles[id]['icon']} size="lg" />
+                                </i>
+                            </div>
                         </MLTooltip>
                     );
                 }
